800f95439276c73f462c4fe805dd407e31a0487f
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-11-22 14:45:47 +0900

    Implement splash activity

---
+++ app/src/main/java/com/ysshin/fine_dust_app/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.ysshin.fine_dust_app
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
+
+class SplashActivity : AppCompatActivity() {
+
+    private val activityScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        activityScope.launch {
+            delay(2000L)
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        activityScope.cancel()
+    }
+}
