c02f850d287b0eeae3b748575e2daf130716d969
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-11-25 14:31:47 +0900

    Implement login function

--- app/src/main/java/com/ysshin/fine_dust_app/data/LoginData.kt
+++ app/src/main/java/com/ysshin/fine_dust_app/data/LoginData.kt
@@ -1,3 +1,8 @@
 package com.ysshin.fine_dust_app.data
 
-data class LoginData()
+import com.google.gson.annotations.SerializedName
+
+data class LoginData(
+    @SerializedName(value = "username") val username: String,
+    @SerializedName(value = "password") val password: String
+)
--- app/src/main/java/com/ysshin/fine_dust_app/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/ysshin/fine_dust_app/viewmodel/LoginViewModel.kt
@@ -1,4 +1,43 @@
 package com.ysshin.fine_dust_app.viewmodel
 
-class LoginViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.ysshin.fine_dust_app.api.AuthService
+import com.ysshin.fine_dust_app.data.AuthData
+import com.ysshin.fine_dust_app.data.LoginData
+import com.ysshin.fine_dust_app.data.LoginRepository
+import retrofit2.Call
+
+class LoginViewModel(authService: AuthService) : ViewModel() {
+    private val repository = LoginRepository(authService)
+
+    val username: MutableLiveData<String> by lazy {
+        MutableLiveData<String>().apply {
+            postValue("")
+        }
+    }
+
+    val password: MutableLiveData<String> by lazy {
+        MutableLiveData<String>().apply {
+            postValue("")
+        }
+    }
+
+    val loading: MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>().apply {
+            postValue(false)
+        }
+    }
+
+    fun setLoading(isLoading: Boolean) {
+        loading.value = isLoading
+    }
+
+    fun clearPassword() {
+        password.value = ""
+    }
+
+    private fun getLoginData(): LoginData = LoginData(username.value ?: "", password.value ?: "")
+
+    fun login(): Call<AuthData> = repository.login(getLoginData())
 }
