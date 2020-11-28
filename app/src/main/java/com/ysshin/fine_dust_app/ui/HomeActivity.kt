59a51e2a20d324cc8eef4bbba3af26d4d920b0c0
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-11-28 22:58:49 +0900

    Add HomeFragment & home navigation

--- app/src/main/java/com/ysshin/fine_dust_app/ui/HomeActivity.kt
+++ app/src/main/java/com/ysshin/fine_dust_app/ui/HomeActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ysshin.fine_dust_app.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
