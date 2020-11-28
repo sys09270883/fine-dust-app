083471e46adab0a98937e48d9e9a1405e8802445
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-11-28 21:40:33 +0900

    Remove viewmodel factories

--- app/src/main/java/com/ysshin/fine_dust_app/viewmodels/LoginViewModelFactory.kt
+++
@@ -1,5 +0,0 @@
-package com.ysshin.fine_dust_app.viewmodels
-
-//class LoginViewModelFactory(private val authService: AuthService): ViewModelProvider.NewInstanceFactory() {
-//    override fun <T : ViewModel?> create(modelClass: Class<T>): T = LoginViewModel(authService) as T
-//}
