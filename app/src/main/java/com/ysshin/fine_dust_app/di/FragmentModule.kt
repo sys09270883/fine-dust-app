45e64a963e87b81d264412d6636f0ff3afc7e5a9
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-11-28 23:28:16 +0900

    Refactor code

--- app/src/main/java/com/ysshin/fine_dust_app/di/FragmentModule.kt
+++ app/src/main/java/com/ysshin/fine_dust_app/di/FragmentModule.kt
@@ -1,5 +1,6 @@
 package com.ysshin.fine_dust_app.di
 
+import com.ysshin.fine_dust_app.ui.HomeFragment
 import com.ysshin.fine_dust_app.ui.LoginFragment
 import com.ysshin.fine_dust_app.ui.RegistrationFragment
 import org.koin.dsl.module
@@ -7,4 +8,5 @@ import org.koin.dsl.module
 val fragmentModule = module {
     factory { LoginFragment() }
     factory { RegistrationFragment() }
+    factory { HomeFragment() }
 }
---
+++ app/src/main/java/com/ysshin/fine_dust_app/viewmodels/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package com.ysshin.fine_dust_app.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.ysshin.fine_dust_app.data.HomeRepository
+
+class HomeViewModel(private val repository: HomeRepository): ViewModel() {
+    // TODO: Make live data.
+
+}
