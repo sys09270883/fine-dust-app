6ac13b47c22237fa3ddf423e61ea24cf05e92d39
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-12-03 00:31:03 +0900

    Disable dark mode in app

--- app/src/main/java/com/ysshin/fine_dust_app/ui/SplashActivity.kt
+++ app/src/main/java/com/ysshin/fine_dust_app/ui/SplashActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.ysshin.fine_dust_app.R
@@ -24,6 +25,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         if (!checkLocationServiceStatus())
             showLocationSettingDialog()
