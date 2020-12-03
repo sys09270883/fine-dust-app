b70eda2b70b6ed33dcb8d63043c9cfdcc0d72fbc
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-12-03 16:16:16 +0900

    Fix Sky parameter

--- app/src/main/java/com/ysshin/fine_dust_app/data/Sky.kt
+++ app/src/main/java/com/ysshin/fine_dust_app/data/Sky.kt
@@ -3,6 +3,6 @@ package com.ysshin.fine_dust_app.data
 import com.google.gson.annotations.SerializedName
 
 data class Sky(
-    @SerializedName(value = "time") val time: Int,
+    @SerializedName(value = "hour") val hour: Int,
     @SerializedName(value = "value") val value: String
 )
